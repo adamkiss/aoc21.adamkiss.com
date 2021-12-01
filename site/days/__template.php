4294de05a89353d097e3fe0a24c05e6378cf4396
Adam Kiss <iam@adamkiss.com>
2021-12-01 17:47:15 +0100

    Fix and prepare template

--- site/days/__template.php
+++ site/days/__template.php
@@ -1,14 +1,17 @@
-<?php
+<?php $input = require_once(kirby()->root('inputs').DS.basename(__FILE__));
+$demoinput = [];
 
-function p1(mixed $input): string
+function p1(mixed $input): mixed
 {
 	return '';
 }
 
-function p2(mixed $input): string
+function p2(mixed $input): mixed
 {
 	return '';
 }
 
-return function () {
-};
+return implode('',[
+	snippet('solution/single', ['demo' => p1($demoinput), 'input' => p1($input), 'label' => 'Result - Part 1'], true),
+	snippet('solution/single', ['demo' => p2($demoinput), 'input' => p2($input), 'label' => 'Result - Part 2'], true)
+]);
